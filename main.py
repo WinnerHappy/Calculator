032d8a5fa1d4a73b04999868f83cf1bba92e6d34
WinnerHappy <Winnerhappydzn@gmail.com>
2022-06-21 14:25:16 -0300

    divisao adicionada

--- main.py
+++ main.py
@@ -14,17 +14,25 @@ def mult(x, y):
 def sub(x, y):
     return x-y
 
-y= input("Informe um Numero:")
-y=int(y)
-
-ope= input("Informe Operador:")
+def div(x, y):
+    return x/y
 
 x= input("Informe um Numero:")
 x=int(x)
 
+ope= input("Informe Operador:")
+
+y= input("Informe um Numero:")
+y=int(y)
+
 if ope == "+": 
     print("A soma é {}".format(sum(x,y)))
+
 elif ope == "*":
     print("A multiplicacao é {}".format(mult(x,y)))
+
 elif ope == "-":
     print("A subtracao é {}".format(sub(x,y)))
+
+elif ope == "/":
+    print("A divisao é {}".format(div(x,y)))
